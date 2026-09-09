b5297fde9ff87c7fb13897f14efd4fe6bae822eb
machot-lam <machotlam@gmail.com>
2026-09-09 15:48:07 +0300

    Add files via upload

--- Week 1.py
+++ Week 1.py
@@ -4,8 +4,8 @@ favorite_language = "JavaScript"
 goal = "To specialize in Python"
 
 print(f"My name is {name}.")
-print(f"I am {28} years old.")
-print(f"My favorite_language is {favorite_language}.")
+print(f"I am {age} years old.")
+print(f"My favorite language is {favorite_language}.")
 print(f"My goal is {goal}.")
 
 # this is introduction variables stored information, while print()
