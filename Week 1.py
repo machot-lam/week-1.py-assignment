61a3d30685c718bf7344ad3266b8ab22cafb9ef7
machot-lam <machotlam@gmail.com>
2026-09-09 15:29:54 +0300

    Add files via upload

---
+++ Week 1.py
@@ -0,0 +1,13 @@
+name = "Machot" # this is name variable
+age = 28 # this is age variable
+favorite_language = "JavaScript"
+goal = "To specialize in Python"
+
+print(f"My name is {name}.")
+print(f"I am {28} years old.")
+print(f"My favorite_language is {favorite_language}.")
+print(f"My goal is {goal}.")
+
+# this is introduction variables stored information, while print()
+# display that information to user
+
